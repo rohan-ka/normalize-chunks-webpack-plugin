be5e261ee86da4ebe835a5a4bfa1057a758f9a78
Rohan Kamat <rohan.ka@media.net>
2017-08-22 19:12:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const { resolve } = require('path');
+const fs = require('fs');
+
+
+class NormalizeChunksPlugin {
+  constructor(options) {
+    const defaultOptions = {
+      filename: 'normalizeChunks.json',
+      path: resolve(process.cwd(), 'build/'),
+    };
+
+    this.options = { ...defaultOptions, ...options };
+  }
+
+  static createAssetMap(assetsByChunkName) {
+    const addChunkToLookup = (lookup, chunk) => {
+      const [name, hash, extension] = chunk.split('.'); // eslint-disable-line no-unused-vars
+
+      return { ...lookup, [`${name}.${extension}`]: chunk };
+    };
+
+    const buildLookup = (assetLookup, chunks) => { return { ...assetLookup, ...chunks.reduce(addChunkToLookup, {}) }; };
+
+    return Object.keys(assetsByChunkName)
+      .map(key => assetsByChunkName[key])
+      .reduce(buildLookup, {});
+  }
+
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, cb) => {
+      const { assetsByChunkName } = compilation.getStats().toJson();
+
+      const assetMap = NormalizeChunksPlugin.createAssetMap(assetsByChunkName);
+      const assetJson = JSON.stringify(assetMap, null, 2);
+
+      this.writeToFile(assetJson).then(() => {
+        compilation.assets[this.options.filename] = { // eslint-disable-line no-param-reassign
+          source() {
+            return assetJson;
+          },
+          size() {
+            return assetJson.length;
+          },
+        };
+      }).then(cb);
+    });
+  }
+
+  writeToFile(content) {
+    return new Promise((res) => {
+      const file = fs.createWriteStream(this.filename(), { mode: 0o755 });
+      file.write(content, 'utf-8', res);
+    });
+  }
+
+  filename() {
+    const { filename, path } = this.options;
+    return `${path}/${filename}`;
+  }
+}
+
+module.exports = NormalizeChunksPlugin;
